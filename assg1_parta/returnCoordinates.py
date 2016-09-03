31b93bdb377d6dc9bc555a04e23a1b8953e22b32
Anupam Sobti <anupamsobti@gmail.com>
2016-09-03 16:32:53 +0530

    finalised function for return coordinates

--- assg1_parta/returnCoordinates.py
+++ assg1_parta/returnCoordinates.py
@@ -3,24 +3,25 @@ import cv2
 import numpy as np
 import sys
 
+minRegionX = 10
+minRegionY = 10
+
 img = cv2.imread(sys.argv[1])
 
 swatches = []
 
 def mouseCallback(event,x,y,flags,param):
-    global swatches,img
+    global swatches,img,startX,startY,endX,endY,minRegionX,minRegionY
     flag = 0
     if event == cv2.EVENT_LBUTTONDOWN:
-        print ("Starts at ",x,y)
         (startX,startY) = (x,y)
-        flag = 1
-    elif flag == 1 and event == cv2.EVENT_MOUSEMOVE:
-        cv2.rectangle(img,(startX,startY),(x,y),(255,0,0),5)
+        print ("Starts at ",startX,startY)
     elif event == cv2.EVENT_LBUTTONUP:
-        print ("Ends at ",x,y)
         endX,endY = x,y
-        #swatches.append((startX,startY,endX,endY))
-        flag = 0
+        print ("Ends at ",x,y)
+        cv2.rectangle(img,(startX,startY),(endX,endY),(255,0,0),3)
+        if ((endX - startX > minRegionX) and (endY - startY > minRegionY)):
+            swatches.append((startX,startY,endX,endY))
 
 # Create a black image, a window and bind the function to window
 cv2.namedWindow('image')
@@ -30,4 +31,6 @@ while(1):
     cv2.imshow('image',img)
     if cv2.waitKey(20) & 0xFF == 27:
         break
+
+print (swatches)
 cv2.destroyAllWindows()
